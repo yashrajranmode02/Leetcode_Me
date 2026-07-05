32ca3db96c7e447a93b6350c87fd747a4ff54bb6
yashrajranmode02 <yashrajranmode02@gmail.com>
2026-07-05 19:20:17 +0530

    Time: 1086 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -2,7 +2,7 @@ SELECT
     CASE 
     when id=(
         select max(id) from Seat
-    ) and id%2%2!=0 then id
+    ) and id%2!=0 then id
     WHEN id%2!=0 then id+1
          WHEN id%2=0 then id-1
          end  

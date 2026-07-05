699c755f066ae5124132446180005c756506e83b
yashrajranmode02 <yashrajranmode02@gmail.com>
2026-07-05 19:19:13 +0530

    Time: 343 ms (87.07%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,11 @@
+SELECT  
+    CASE 
+    when id=(
+        select max(id) from Seat
+    ) and id%2%2!=0 then id
+    WHEN id%2!=0 then id+1
+         WHEN id%2=0 then id-1
+         end  
+         as id,student 
+FROM Seat 
+order by id

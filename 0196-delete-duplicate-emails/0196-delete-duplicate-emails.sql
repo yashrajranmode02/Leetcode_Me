a0009fe14fb483738469b06feb4bf3cbf350f076
yashrajranmode02 <yashrajranmode02@gmail.com>
2026-07-14 19:03:36 +0530

    Time: 885 ms (45.9%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,17 +1,5 @@
--- # Write your MySQL query statement below
-with cte as(
-    select *,
-    RANK() over(
-         partition by email   
-         order by id   
-    )as rd
-    from Person 
-)
-delete from Person 
-where id in (
-    select id from cte 
-    where rd>1
-)
--- where rd>1
-
--- delete from Person where (id,email) not in (select min(id) as id , email from Person group by email)
+# Write your MySQL query statement below
+delete p2
+from Person p1
+join Person p2 
+on p1.email=p2.email and p1.id<p2.id

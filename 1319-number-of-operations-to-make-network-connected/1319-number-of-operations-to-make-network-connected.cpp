2223819fc72738757aa9bf3b8c45a87cbf62d9a5
yashrajranmode02 <yashrajranmode02@gmail.com>
2026-08-10 13:50:24 +0530

    Time: 44 ms (12.07%), Space: 72.3 MB (5.01%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int ExtraEdges = 0;
+    void BFS(int i, vector<vector<int>>& adjacency_list, vector<int>& Visited) {
+        queue<int> q;
+        q.push(i);
+        int edges = 0;
+        int Vertex = 0;
+        Visited[i] = 1;
+        while (!q.empty()) {
+            int node = q.front(); 
+            Vertex++;
+            q.pop(); 
+            for (int i = 0; i < adjacency_list[node].size(); i++) {
+                edges++;
+                if (!Visited[adjacency_list[node][i]]) {
+                    q.push(adjacency_list[node][i]);
+                    Visited[adjacency_list[node][i]] = 1;
+                }
+            }
+        } 
+        edges/=2;
+        if (edges - (Vertex - 1) >= 0) {
+            ExtraEdges += (edges - (Vertex - 1));
+        }
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        vector<vector<int>> adjacency_list(n, vector<int>());
+        for (int i = 0; i < connections.size(); i++) {
+            adjacency_list[connections[i][0]].push_back(connections[i][1]);
+            adjacency_list[connections[i][1]].push_back(connections[i][0]);
+        }
+
+        int component = 0;
+        vector<int> Visited(n, 0);
+        for (int i = 0; i < n; i++) {
+            if (!Visited[i]) {
+                component++;
+                BFS(i, adjacency_list, Visited);
+            }
+        }
+        // cout<<ExtraEdges;
+        if (ExtraEdges >= component - 1)
+            return component - 1;
+        return -1;
+    }
+};

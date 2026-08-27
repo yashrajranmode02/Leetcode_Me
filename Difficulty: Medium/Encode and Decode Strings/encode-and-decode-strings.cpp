be3a53e98661b660c7c6c448df59d2765aa86e71
yashrajranmode02 <yashrajranmode02@gmail.com>
2026-08-27 22:40:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Encode and Decode Strings/encode-and-decode-strings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+  string d="";
+  vector<string>ed;
+    string encode(vector<string>& arr) {
+        ed=arr;
+        return d;
+    }
+
+    vector<string> decode(string& s) {
+        // write your logic to decode the string
+        d=s;
+        return ed;
+    }
+};

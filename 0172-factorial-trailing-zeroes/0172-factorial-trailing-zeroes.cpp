286c72f4c413dd79494c163c154835d5c82228af
yashrajranmode02 <yashrajranmode02@gmail.com>
2026-08-27 22:20:23 +0530

    Time: 0 ms (100%), Space: 8 MB (87.06%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int d=-1;int fivisor=5;int c=0;
-        while(d!=0){
-            d=(n/fivisor);c+=d;fivisor*=5;
+        int c = 0;
+        int d = -1;
+        int fivisor = 5;
+        while (d != 0) {
+            d = n / fivisor;
+            c+=d;fivisor*=5;
         }
         return c;
     }
